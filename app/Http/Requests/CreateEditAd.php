07aaa7778e3b2aaad0c65cb4431fdcbeb30e595c
Dev <silent04222@gmail.com>
2020-02-27 23:55:15 +0200

    Work changes synchronized

---
+++ app/Http/Requests/CreateEditAd.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CreateEditAd extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'title' => 'required|between:3,50',
+            'description' => 'required',
+            'state' => Rule::in(['bad', 'average', 'good', 'excellent']),
+            'price' => 'numeric',
+        ];
+
+        if($this->has('id')) {
+            $rules += ['id' => 'exists:ads,id'];
+        }
+
+        return $rules;
+    }
+}
